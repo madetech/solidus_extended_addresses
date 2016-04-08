5ec97003ff841a49a1044d24e6ec249114cab5c7
Seb Ashton <seb@sebashton.com>
2016-04-08 11:27:22 +0100

    Add title to permitted address_attributes

--- lib/solidus_extended_addresses/engine.rb
+++ lib/solidus_extended_addresses/engine.rb
@@ -13,6 +13,8 @@ module SolidusExtendedAddresses
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+
+      Spree::PermittedAttributes.address_attributes << :title
     end
 
     config.to_prepare(&method(:activate).to_proc)
